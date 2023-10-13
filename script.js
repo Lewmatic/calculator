0ed3424a66f7c54de0d64bb338cd63c4b4ca33ea
Lewmatic <mwl2.2000@gmail.com>
2023-10-12 21:19:27 -0500

    Added functionality to number buttons
     - Pressing numbers buttons makes them appear in the output window.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("button");
+const output = document.getElementById("output-value");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -6,5 +7,19 @@ buttons.forEach((button) => {
     setTimeout(() => {
       button.classList.remove("click");
     }, 80);
+
+    if (
+      button.textContent !== "AC" &&
+      button.textContent !== "C" &&
+      button.textContent !== "=" &&
+      button.textContent !== "+" &&
+      button.textContent !== "-" &&
+      button.textContent !== "×" &&
+      button.textContent !== "÷"
+    ) {
+      output.textContent += button.textContent;
+    }
   });
 });
+
+
